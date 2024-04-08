0bcb812d4c65fc42c38ed352fbc4c4946a0c5655
ENERGIUM <martintabuss@gmail.com>
2024-04-08 10:02:15 -0300

    Initial Commit - Upload branch master

---
+++ Practico2/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Data;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese Numero: ");
+            string data = Console.ReadLine();
+            string invNumero = "";
+            int digito = 0;
+            //Console.ReadKey();
+            bool siConvertible = int.TryParse(data, out int numero);
+            numero = siConvertible ? Convert.ToInt32(data) : 0;
+            if (siConvertible)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    digito = numero % 10;
+                    numero = (numero / 10);
+                    invNumero = invNumero + Convert.ToString(digito);
+                }
+                Console.Write("Numero Invertido: ");
+                Console.WriteLine(invNumero);
+            }
+            else Console.WriteLine("Numero no valido");
+        }
+    }
+}
