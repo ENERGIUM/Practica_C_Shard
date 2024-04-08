2e37aaf2311383d100ca5898780d5361f2176591
ENERGIUM <martintabuss@gmail.com>
2024-04-08 14:31:47 -0300

    Created file .gitignore

--- Practico2/Ejercicio2/Ejercicio2/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
+++ Practico2/Ejercicio2/Ejercicio2/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506ee3c0bc02a071c1785b3abfc2001ef54af157")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
