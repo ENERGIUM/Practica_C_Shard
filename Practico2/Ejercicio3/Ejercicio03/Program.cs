9b1a23e9e1eff8ae36355847c5cb1c3226e7a998
ENERGIUM <martintabuss@gmail.com>
2024-04-09 17:56:57 -0300

    Upload Ejercicio2 y Ejercicio3 completo

---
+++ Practico2/Ejercicio3/Ejercicio03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            string x = Console.ReadLine();
+            double num = double.Parse(x);
+
+            if(num == comprobar(x)){
+                Console.WriteLine("El numero ingresado es un número de Armstrong");
+            }else{
+                Console.WriteLine("El numero ingresado no es un número de Armstrong");
+            }
+
+        }
+
+        public static double comprobar(String x)
+        {
+            int[] cifras = new int[x.Length];
+            int i = 0;
+            foreach(Char cifra in x){ 
+                cifras[i]= int.Parse(cifra.ToString());
+                i++;
+            }
+
+            double contador = 0;
+            foreach(int cifra in cifras){
+                double j = Math.Pow( cifra, cifras.Length);
+                contador += j;
+            }
+
+            return contador;
+        }
+    }
+}
