c3dbc2aef4e0383f277d8b91e8c49a33387ee2c4
ENERGIUM <martintabuss@gmail.com>
2024-04-09 21:30:24 -0300

    EJERCICIOS 6 Y 7

---
+++ Practico2/Ejercicio06/Ejercicio06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Ejercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un caracter");
+            String entrada = Console.ReadLine();
+            Char letra = char.Parse(entrada.ToLower());
+
+            bool flag = int.TryParse(entrada, out int sal1);
+           
+            if (flag) {
+                Console.WriteLine("El caracter es una cifra numerica");
+            }else {
+
+                switch (letra)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        Console.WriteLine("El caracter es una vocal");
+                        break;
+                    default:
+                        if ((double)letra > 96 && (double)letra < 123)
+                        {
+                            Console.WriteLine("El caracter es una consonante");
+                        }else{
+                            Console.WriteLine("Caracter invalido"); }
+                        break;
+                    }
+                }
+        }   
+        
+    }
+}
---
+++ Practico2/Ejercicio07/Ejercicio07/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejercicio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese la base del triangulo");
+            String x= Console.ReadLine();
+            Console.WriteLine("Ingrese la altura del triangulo");
+            String y = Console.ReadLine();
+
+            double Base = double.Parse(x), Altura = double.Parse(y), resultado;
+
+            double hipotenusa = Math.Pow(Altura,2) + Math.Pow(Base,2);
+            hipotenusa = Math.Sqrt(hipotenusa);
+
+            double area = (Base * Altura)/2;
+
+            String linea = String.Format("{0:####.00}", hipotenusa);
+            Console.WriteLine("Hipotenusa = " + linea);
+            linea = String.Format("{0:####.00}", area);
+            Console.WriteLine("Area = " + linea);
+            
+
+           
+        }
+    }
+}
