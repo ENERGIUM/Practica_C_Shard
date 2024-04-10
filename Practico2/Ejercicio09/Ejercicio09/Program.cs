b3b9f54a7b7ee0a62ed4fb87c9cd3bfd2f5e9f6f
= <=>
2024-04-10 16:58:05 -0300

    Upload Ejercicios 9 y 10

---
+++ Practico2/Ejercicio09/Ejercicio09/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ejercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese la cantidad de numeros que desea generar");
+            String entrada = Console.ReadLine();
+
+            byte cantidadNro = byte.Parse(entrada);
+
+            Random ran = new Random();
+
+            for(int i =0; i < cantidadNro; i++)
+            {
+                int Nro = ran.Next(100);
+                Console.WriteLine("Numero generado: " + Nro + " Codigo ASCII: " + (char)Nro);
+            }
+        }
+    }
+}
---
+++ Practico2/Ejercicio10/Ejercicio10/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml.Schema;
+
+namespace Ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre");
+            String SuNombre = Console.ReadLine();
+
+            byte k = (byte)SuNombre.Length;
+
+            Console.WriteLine("La LONGITUD de su nombre es: " + k);
+
+            if (SuNombre.Contains("le")) {
+                Console.WriteLine("La sibala 'le' ESTA DENTRO de su nombre");
+            }else {
+                Console.WriteLine("La sibala 'le' No ESTA DENTRO de su nombre");}
+
+            Console.WriteLine("REEMPLAZO DE VOCAL A por b: " + SuNombre.ToLower().Replace('a', 'b'));
+            Console.WriteLine("MINUSCULAS: " + SuNombre.ToLower());
+            Console.WriteLine("MINUSCULAS: " + SuNombre.ToUpper());
+            String resultado = SuNombre.Remove(SuNombre.Length-4, 4);
+            Console.WriteLine("REMOVEMOS 4 LETRAS: " + resultado);
+            Console.WriteLine("EXTRAEMMOS ultimas 4 LETRAS: " + SuNombre.Substring(SuNombre.Length - 4));
+            Console.WriteLine("Recorremos los caracteres desde la Izquierda: ");
+            recorrerCharIzq(SuNombre);
+            Console.WriteLine("Recorremos los caracteres desde la derecha: ");
+            recorrerCharDer(SuNombre);
+        }
+
+
+        public static string Left(string param, int length)
+        {
+            string result = param.Substring(0, length);
+            return result;
+        }
+
+        public static string Right(string param, int length)
+        {
+            int value = param.Length - length;
+            string result = param.Substring(value, length);
+            return result;
+        }
+
+        public static void recorrerCharIzq(string param)
+        {
+            String salida = " ";
+            for(int i=0; i<param.Length; i++)
+            {
+                salida = salida + param.Substring(i, 1);
+                Console.WriteLine(salida);
+
+            } 
+        }
+
+        public static void recorrerCharDer(string param)
+        {
+            String salida = " ";
+            for (int i = param.Length-1; i >=0; i--)
+            {
+                salida = param.Substring(i, 1) + salida;
+                Console.WriteLine(salida);
+
+            }
+        }
+    }
+}
