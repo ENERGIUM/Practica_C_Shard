613191c0f4fcec9c9b328013b1af171dc272a0ed
= <=>
2024-04-22 11:24:12 -0300

    Upload Ejercicio3

---
+++ Practico3/Ejercicio3/Ejercicio3/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Constantes
+    {
+        public static string NOMBRE_APLICACION = "Sistema de control de deposito Zona Derecha Sur";
+        public static string VERSION = "3.0.9";
+        public static string RUTA_ARCHIVO_LOG = "C:\\Users\\Usuario\\AppData\\Local\\Sistema_De_Control_Deposito";
+    }
+}
---
+++ Practico3/Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre de aplicacion: "+ Constantes.NOMBRE_APLICACION);
+            Console.WriteLine("Version: " + Constantes.VERSION);
+            Console.WriteLine("Ubicacion de archivo LOG = " + Constantes.RUTA_ARCHIVO_LOG);
+        }
+    }
+}
