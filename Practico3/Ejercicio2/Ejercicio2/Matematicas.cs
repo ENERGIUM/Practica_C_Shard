272cbc8bdbe891a7a94f617117b66748d01b7d9c
= <=>
2024-04-22 11:11:27 -0300

    Upload Ejercicio2

---
+++ Practico3/Ejercicio2/Ejercicio2/Matematicas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    static class Matematicas
+    {
+        public static double pi = 3.14159;
+        public static int sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
---
+++ Practico3/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23, b = 45;
+            Console.WriteLine("Resultado: "+Matematicas.sumar(a,b));
+            Console.WriteLine("Valor de pi: " + Matematicas.pi);
+        }
+    }
+}
